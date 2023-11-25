f16bde2fbc911355958b557fe144b6ee4313de15
ThiagoFabiano <thiagoffabiano@gmail.com>
2023-11-25 20:22:57 -0300

    feat: novas funções

--- main.py
+++ main.py
@@ -5,8 +5,14 @@ def main():
         usuarios = pickle.load(file);
         conexoes = pickle.load(file);
         historico = pickle.load(file);
+    
+    conexoes_atualizadas = gerarConexoes(historico, conexoes)
 
-    print(gerarConexoes(historico, conexoes));
+    with open('dados.bin', 'wb') as file:
+        pickle.dump(usuarios, file)
+        pickle.dump(conexoes_atualizadas, file) 
+    
+    estatisticas(usuarios,  conexoes_atualizadas)
 
 
 def gerarConexoes(historico, conexoes):
@@ -42,5 +48,64 @@ def gerarConexoes(historico, conexoes):
                 conexoes[conexao[1]][2].append(conexao[0])
     return conexoes;
 
+def anterior(x,y, usuarios, conexoes_atualizadas):
+
+    if(usuarios[x][1] < usuarios[y][1]): return True
+    if(usuarios[x][1] > usuarios[y][1]): return False
+
+    if(len(conexoes_atualizadas[x][1]) > len(conexoes_atualizadas[x][1])): return True
+    if(len(conexoes_atualizadas[x][1]) > len(conexoes_atualizadas[x][1])): return False
+
+    if( x < y): return True 
+
+    return False
+
+def particao(l, inf , sup, usuarios, conexoes_atualizadas ): # inf sup primeiro e ultimo item
+    pivot = l[inf]
+    i = inf +1
+    j = sup
+
+    while i <= j:
+        while i <= j and anterior( l[i] , pivot , usuarios, conexoes_atualizadas):
+            i += 1
+        while j >= i and not anterior( l[j] , pivot , usuarios, conexoes_atualizadas):
+            j -= 1
+
+        if i < j: l[i], l[j] = l[j], l[i]
+
+    l[inf], l[j] = l[j], l[inf]
+    return j
+    
+def quickSort(l, inf , sup , usuarios, conexoes_atualizadas ):
+    if inf < sup: 
+        pos = particao(l, inf , sup , usuarios, conexoes_atualizadas)
+        quickSort(l, inf, pos-1, usuarios, conexoes_atualizadas)
+        quickSort(l, pos+1, sup, usuarios, conexoes_atualizadas)
+    
+    
+
+
+def estatisticas(usuarios,  conexoes_atualizadas):
+    logins = list(usuarios.keys())
+    
+    quickSort(logins, 0, len(logins) - 1, usuarios, conexoes_atualizadas)
+    print(f"{len(logins)}")
+    listaCidadeUsuario = []
+    for i in logins:
+        listaCidadeUsuario.append((usuarios[i][1], usuarios[i][0]))
+    melhorDeCadaCidade = []
+    for index in range(len(listaCidadeUsuario)-1):
+        if index != 0:
+            if listaCidadeUsuario[index][0] != listaCidadeUsuario[index-1][0]:
+                melhorDeCadaCidade.append(listaCidadeUsuario[index])
+        else: melhorDeCadaCidade.append(listaCidadeUsuario[index])
+    
+    for i in melhorDeCadaCidade:
+        print(f"{i[0]} {i[1]}")
+ 
+
+
 if __name__ == "__main__":
     main()
+
+
